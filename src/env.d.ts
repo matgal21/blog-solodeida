6f3ad779ddbc36f6de9a5e2cf40f0237e19db4d8
Matgal <chinogalvez5@gmail.com>
2024-08-14 17:57:03 -0300

    navbar

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 declare module "preline/plugin";
+// types/astro-navbar.d.ts
+declare module "astro-navbar" {
+    export const Astronav: any;
+    export const MenuItems: any;
+    export const MenuIcon: any;
+    export const Dropdown: any;
+    export const DropdownItems: any;
+    export const DropdownSubmenu: any;
+  }

1dc65eb897023f84b231e66135ec218b66c07cdd
Matgal <chinogalvez5@gmail.com>
2024-09-10 00:13:50 -0300

    Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import partytown from "@astrojs/partytown";
 import pagefind from "astro-pagefind";
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 
 
@@ -14,6 +15,11 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://solodeida.vercel.app/",
   trailingSlash: "always",
+
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 
   prefetch: {
     prefetchAll: true,
